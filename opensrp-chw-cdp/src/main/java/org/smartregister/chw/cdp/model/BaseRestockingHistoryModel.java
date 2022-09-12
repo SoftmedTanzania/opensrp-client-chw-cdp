7d88a863a4cc79778781784708bcae4eec29c0e9
zk497 <zameerkh12377@gmail.com>
2022-09-12 12:17:17 +0300

    Passing current number of condom stock to the form

--- opensrp-chw-cdp/src/main/java/org/smartregister/chw/cdp/model/BaseRestockingHistoryModel.java
+++ opensrp-chw-cdp/src/main/java/org/smartregister/chw/cdp/model/BaseRestockingHistoryModel.java
@@ -1,13 +1,20 @@
 package org.smartregister.chw.cdp.model;
 
+import static org.smartregister.chw.cdp.dao.CdpStockingDao.getCurrentFemaleCondomCount;
+import static org.smartregister.chw.cdp.dao.CdpStockingDao.getCurrentMaleCondomCount;
+
 import org.json.JSONObject;
 import org.smartregister.chw.cdp.contract.RestockingHistoryContract;
 import org.smartregister.chw.cdp.util.CdpJsonFormUtils;
+import org.smartregister.util.Utils;
 
 public class BaseRestockingHistoryModel implements RestockingHistoryContract.Model {
     @Override
     public JSONObject getFormAsJson(String formName, String entityId) throws Exception {
         JSONObject form = CdpJsonFormUtils.getFormAsJson(formName);
+        String userLocationId = Utils.getAllSharedPreferences().fetchUserLocalityId(Utils.getAllSharedPreferences().fetchRegisteredANM());
+        form.getJSONObject("global").put("male_condom_count", getCurrentMaleCondomCount(userLocationId));
+        form.getJSONObject("global").put("female_condom_count", getCurrentFemaleCondomCount(userLocationId));
         CdpJsonFormUtils.getRegistrationForm(form, entityId, "");
         return form;
     }
